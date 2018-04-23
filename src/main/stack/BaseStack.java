d41fa4551881a1755c30d927a51821d43af9be06
wangjing9 <wangjing9@xiaomi.com>
2018-04-23 22:50:47 +0800

    add a tack question, OneStackSortAnotherStack

--- src/main/stack/BaseStack.java
+++ src/main/stack/BaseStack.java
@@ -11,12 +11,12 @@ public abstract class BaseStack {
 
     static {
         stack = new Stack<>();
+        stack.push(5);
         stack.push(1);
-        stack.push(2);
+        stack.push(6);
+        stack.push(4);
         stack.push(3);
-//        stack.push(4);
-//        stack.push(5);
-//        stack.push(6);
+        stack.push(2);
     }
 
     public static void print() {
