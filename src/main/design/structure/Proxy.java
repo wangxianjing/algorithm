0f17aa6b2a3baba079859788599b5a88e13b963b
wangjing9 <wangjing9@xiaomi.com>
2018-07-02 20:54:17 +0800

    add dynamic proxy

--- src/main/design/structure/Proxy.java
+++
@@ -1,9 +0,0 @@
-package main.design.structure;
-
-/**
- * Created by wong on 18/7/2.
- */
-public class Proxy {
-
-
-}
---
+++ src/main/design/structure/proxy/dynamic/TargetInterface.java
@@ -0,0 +1,8 @@
+package main.design.structure.proxy.dynamic;
+
+/**
+ * Created by wong on 18/7/2.
+ */
+public interface TargetInterface {
+    public void play();
+}
