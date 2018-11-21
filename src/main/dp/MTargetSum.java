d27a0f7febc2da66b82a23d28aa138c4183db4d5
wangjing <jing.wang1@fengjr.com>
2018-11-21 09:11:26 +0800

    add MTargetSum

---
+++ src/main/dp/MTargetSum.java
@@ -0,0 +1,29 @@
+package main.dp;
+
+/**
+ * Created by wong on 2018/11/20.
+ */
+public class MTargetSum {
+    static int count = 0;
+
+    public static int findTargetSumWays(int[] nums, int S) {
+        solution(nums, S, 0, 0);
+        return count;
+    }
+
+    public static void solution(int[] nums, int S, int i, int sum) {
+        if (i == nums.length) {
+            if (sum == S) {
+                count++;
+            }
+        } else {
+            solution(nums, S, i+1, sum - nums[i]);
+            solution(nums, S, i+1, sum + nums[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{1, 1, 1, 1, 1};
+        System.out.println(findTargetSumWays(a, 3));
+    }
+}
