cc15e126b130795b4484e0037adb5eae8fd1dd2e
wangjing <jing.wang1@fengjr.com>
2018-11-26 19:05:26 +0800

    add EValidParentheses

---
+++ src/main/java/SerialDemo.java
@@ -0,0 +1,73 @@
+package main.java;
+
+import java.io.*;
+
+/**
+ * Created by wong on 2018/11/22.
+ */
+public class SerialDemo {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        //序列化
+        FileOutputStream fos = new FileOutputStream("/Users/wangjing/tmp/object.out");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        User user1 = new User("xuliugen", "123456", "male");
+        oos.writeObject(user1);
+        oos.flush();
+        oos.close();
+        //反序列化
+        FileInputStream fis = new FileInputStream("/Users/wangjing/tmp/object.out");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        User user2 = (User) ois.readObject();
+        System.out.println(user2.getUserName()+ " " +
+                user2.getPassword() + " " + user2.getSex());
+        //反序列化的输出结果为：xuliugen 123456 male
+    }
+    public static class User implements Serializable {
+        private String userName;
+        private String password;
+        private String sex;
+        private static int anInt=32;
+
+        public User(String userName, String password, String sex) {
+            this.userName = userName;
+            this.password = password;
+            this.sex = sex;
+        }
+        //全参构造方法、get和set方法省略
+
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getSex() {
+            return sex;
+        }
+
+        public void setSex(String sex) {
+            this.sex = sex;
+        }
+
+        public static int getAnInt() {
+            return anInt;
+        }
+
+        public static void setAnInt(int anInt) {
+            User.anInt = anInt;
+        }
+    }
+
+}
---
+++ src/main/sstring/EValidParentheses.java
@@ -0,0 +1,52 @@
+package main.sstring;
+
+import java.util.Stack;
+
+/**
+ * Example 2:
+ * <p>
+ * Input: "()[]{}"
+ * Output: true
+ * Example 3:
+ * <p>
+ * Input: "(]"
+ * Output: false
+ * Created by wong on 2018/11/26.
+ */
+public class EValidParentheses {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char curChar = s.charAt(i);
+            if (stack.isEmpty()) {
+                stack.push(curChar);
+            } else {
+                Character topChar = stack.peek();
+                if (isPairChars(curChar, topChar) || isPairChars(topChar, curChar)) {
+                    stack.pop();
+                } else {
+                    stack.push(curChar);
+                }
+            }
+        }
+        if (stack.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isPairChars(char a, char b) {
+        if ((a == '{' && b == '}') || (a == '(' && b == ')') || (a == '[' && b == ']')) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s1= "()[]{}";
+        System.out.println(isValid(s1));
+    }
+}
