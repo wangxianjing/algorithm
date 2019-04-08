07a01f82f064ec0352db7891457424339f4cc35b
wangjing <jing.wang1@fengjr.com>
2019-04-08 23:29:50 +0800

    新增外观模式

---
+++ src/main/design/structure/facadePattern/Circle.java
@@ -0,0 +1,11 @@
+package main.design.structure.facadePattern;
+
+/**
+ * Created by wong on 2019/4/8.
+ */
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("draw a circle");
+    }
+}
---
+++ src/main/design/structure/facadePattern/Client.java
@@ -0,0 +1,14 @@
+package main.design.structure.facadePattern;
+
+/**
+ * 为复杂的子系统或者模块提供界面访问的模块，子系统之间耦合性小
+ * Created by wong on 2019/4/8.
+ */
+public class Client {
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
