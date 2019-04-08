1a5ae4fba4a779a40ea5292f902bc365e5a35eb9
wangjing <jing.wang1@fengjr.com>
2019-04-08 17:14:01 +0800

    新增原型模式

---
+++ src/main/design/creational/protoType/Square.java
@@ -0,0 +1,15 @@
+package main.design.creational.protoType;
+
+/**
+ * Created by wong on 2019/4/8.
+ */
+public class Square extends Shape {
+    @Override
+    protected void draw() {
+        System.out.println("Draw a circle");
+    }
+
+    public Square() {
+        type = "Square";
+    }
+}
